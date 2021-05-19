520c56b2e363edb3108d8286394caeff67c89db3
udhos <udhos>
2021-05-19 01:17:03 -0300

    More tests.

--- bulexer/lexer_test.go
+++ bulexer/lexer_test.go
@@ -37,7 +37,17 @@ var testTable = []lexerTest{
 	{"compare4 invalid unequal", "><", "GT(>) LT(<) EOF()"},
 	{"compare5 double equals", "==", "EQ(=) EQ(=) EOF()"},
 	{"exclamation is not valid symbol", "!", "ERROR(error-after-unexpected:!)"},
-	{"all valid symbols", "\n true false AND OR NOT CONTAINS CurrentTime Number List myVar123 123 'not long text' ( ) [ ] < > != = <= >= ", "KW-TRUE(true) KW-FALSE(false) KW-AND(AND) KW-OR(OR) KW-NOT(NOT) KW-CONTAINS(CONTAINS) KW-CURRENTTIME(CurrentTime) KW-NUMBER(Number) KW-LIST(List) IDENT(myVar123) NUMBER(123) TEXT(not long text) LPAR(() RPAR()) LSBKT([) RSBKT(]) LT(<) GT(>) NE(!=) EQ(=) LE(<=) GE(>=) EOF()"},
+	{"all valid symbols", "\n true false AND OR NOT CONTAINS CurrentTime Number List myVar123 123 'not long text' ( ) [ ] < > != = <= >= ",
+		"KW-TRUE(true) KW-FALSE(false) KW-AND(AND) KW-OR(OR) KW-NOT(NOT) KW-CONTAINS(CONTAINS) " +
+			"KW-CURRENTTIME(CurrentTime) KW-NUMBER(Number) KW-LIST(List) IDENT(myVar123) NUMBER(123) " +
+			"TEXT(not long text) LPAR(() RPAR()) LSBKT([) RSBKT(]) LT(<) GT(>) NE(!=) EQ(=) LE(<=) GE(>=) EOF()"},
+	{"basic expression 1", "a=b", "IDENT(a) EQ(=) IDENT(b) EOF()"},
+	{"basic expression 2", "1=1", "NUMBER(1) EQ(=) NUMBER(1) EOF()"},
+	{"basic expression 3", "'1'='a'", "TEXT(1) EQ(=) TEXT(a) EOF()"},
+	{"basic expression 4", "true=false", "KW-TRUE(true) EQ(=) KW-FALSE(false) EOF()"},
+	{"long expression", "NOT [ 1 b 'c' ] NOT CONTAINS Number('3')",
+		"KW-NOT(NOT) LSBKT([) NUMBER(1) IDENT(b) TEXT(c) RSBKT(]) KW-NOT(NOT) KW-CONTAINS(CONTAINS) KW-NUMBER(Number) LPAR(() TEXT(3) RPAR()) EOF()"},
+	{"can concat some symbols", "true'text'123(var'text2'false", "KW-TRUE(true) TEXT(text) NUMBER(123) LPAR(() IDENT(var) TEXT(text2) KW-FALSE(false) EOF()"},
 }
 
 func TestScanner(t *testing.T) {
